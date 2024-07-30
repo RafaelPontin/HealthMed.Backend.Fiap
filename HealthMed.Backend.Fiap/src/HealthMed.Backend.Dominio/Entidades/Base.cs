645055cc3a12d27a2cf9d466f9f045e826f89fa9
rafael pontin <rfmpontin@gmail.com>
2024-07-29 21:57:13 -0300

    feat: ajuste na classe base para validacao de erros

--- HealthMed.Backend.Fiap/src/HealthMed.Backend.Dominio/Entidades/Base.cs
+++ HealthMed.Backend.Fiap/src/HealthMed.Backend.Dominio/Entidades/Base.cs
@@ -3,14 +3,22 @@
     public abstract class Base
     {
         public Guid Id { get; private set; }
+        public IList<string> Erros { get; private set; }
 
         public Base()
         {
             Id = Guid.NewGuid();
+            Erros = new List<string>();
         }
         protected void DefinirId(Guid id)
         {
             Id = id;
+        }
+
+        protected void AddErro(string erro)
+        {
+            if (!string.IsNullOrWhiteSpace(erro))
+                Erros.Add(erro);
         }
     }
 }
